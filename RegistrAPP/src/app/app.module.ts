4dd9552474a188c2caa41d70fa9fdebcd3ba0c80
AlexD30 <yer.vasquez@duocuc.cl>
2022-09-03 09:41:41 -0400

    Slider

--- RegistrAPP/src/app/app.module.ts
+++ RegistrAPP/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  
 })
 export class AppModule {}
