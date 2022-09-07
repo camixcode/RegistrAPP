80d6f112017826cb9d243f7e1d61058e794abb5c
AlexD30 <yer.vasquez@duocuc.cl>
2022-09-07 09:42:54 -0300

    Asistencia

--- RegistrAPP/src/app/app-routing.module.ts
+++ RegistrAPP/src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'asistencia',
+    loadChildren: () => import('./asistencia/asistencia.module').then( m => m.AsistenciaPageModule)
   },
+
 
 
 ];
